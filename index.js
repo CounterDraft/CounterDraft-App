10f62cb7e9035f825cbb059e5fe3da0882d8ce11
Jerum Hubbert <hubbertj@gmail.com>
2015-09-29 10:15:54 -0700

    trying to just get the build to work again.

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.set('port', (process.env.PORT || 8080));
 
 app.use(expressLayouts);
 
-routes.setup(app);
+// routes.setup(app);
 
 app.listen(app.get('port'), function() {
         console.log('Node app is running on port', app.get('port'));
