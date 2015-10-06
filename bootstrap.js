a230870d81c8f9c50c9c4c84b0edea69eaf9daec
Jerum Hubbert <hubbertj@gmail.com>
2015-10-05 19:14:55 -0700

    TO-0221518
    changed the structure of the index.

--- bootstrap.js
+++ bootstrap.js
@@ -5,108 +5,82 @@ var DBCreateConnection = function(config) {
 };
 
 module.exports = {
-	setupGlobals: function() {
-		// Setup the configuration
-		GLOBAL.config = require('./config/config');
+    init: function() {
+        GLOBAL.getUtil = require('util');
+        // Setup the configuration
+        GLOBAL.config = require('./config/config');
 
-		GLOBAL.BASE_URL = 'http://' + GLOBAL.config.server.ip + ':' + GLOBAL.config.server.port + '/';
-		GLOBAL.CONTROLLER_DIR = './app/controller/';
-		GLOBAL.MODEL_DIR = './app/model/';
-		
-		GLOBAL.Promise = require('promise');
+        GLOBAL.smtpTransport = require("nodemailer").createTransport(GLOBAL.config.email);
+        GLOBAL.BASE_URL = 'http://' + GLOBAL.config.server.ip + ':' + GLOBAL.config.server.port + '/';
+        GLOBAL.CONTROLLER_DIR = './app/controller/';
+        GLOBAL.MODEL_DIR = './app/model/';
 
-		GLOBAL.generateUUID = function() {
-			var uuid = require('uuid');
-			return uuid.v4();
-		};
-		
-		GLOBAL.getAuthorization = function() {
-			return require('express-authorization');
-		};
-		
-		GLOBAL.getExpressSession = function() {
-			return require('express-session');
-		};
+        GLOBAL.Promise = require('promise');
 
-		GLOBAL.getController = function(controllerName) {
-			var Controller = require(GLOBAL.CONTROLLER_DIR + controllerName);
-			return new Controller();
-		};
+        GLOBAL.generateUUID = function() {
+            var uuid = require('uuid');
+            return uuid.v4();
+        };
 
-		GLOBAL.getModel = function(modelName) {
-			var Model = require(GLOBAL.MODEL_DIR + modelName);
-			return new Model();
-		};
-		
-		GLOBAL.getDBConnection = function() {
-			var mysql = require('mysql');
-			var dbconfig = GLOBAL.config.database;
-			var connection = mysql.createConnection(dbconfig);
-			return connection;
-		};
-		
-		GLOBAL.generatePasswordHash = function(password, salt) {
-			var shasum = GLOBAL.getSHA1();
-			shasum.update(salt + password + salt);
-			var hash = shasum.digest('hex');
-			return hash;
-		};
+        GLOBAL.getAuthorization = function() {
+            return require('express-authorization');
+        };
 
-		GLOBAL.getCrypt = function() {
-			return require('crypto');
-    	};
-		
-		GLOBAL.getSHA1 = function() {
-			var crypt = GLOBAL.getCrypt();
-			var shasum = crypt.createHash('sha1');
-			return shasum;
-		};
-		
-		GLOBAL.getValidator = function() {
-			var validator = require('validator');
-			validator.extend('isPassword', function(str) {
-				// TODO: Finish writing the regex to test passwords
-				if(!str) {
-					return false;
-				}
-				return true;
-				//return /^$/.test(str);
-			});
-			return new Validator();
-		};
+        GLOBAL.getExpressSession = function() {
+            return require('express-session');
+        };
 
-		GLOBAL.getDateFormatter = function() {
-			return require('dateformat');
-		};
-		
-		GLOBAL.smtpTransport = require("nodemailer").createTransport(GLOBAL.config.email);
-		GLOBAL.logger = require('./lib/logger');
-	},
-	
-	setupApp: function(app, express) {
-		var cookieParser = require('cookie-parser');
-		var bodyParser = require('body-parser');
-		var cors = require('./lib/cors');
-		var sessionFactory = require('./lib/sessionFactory');
-		var expressLayouts = require('express-ejs-layouts');
+        GLOBAL.getController = function(controllerName) {
+            var Controller = require(GLOBAL.CONTROLLER_DIR + controllerName);
+            return new Controller();
+        };
 
-		app.use(express.static(__dirname));
-		app.use(bodyParser.urlencoded( { extended: true } ));
-		app.use(bodyParser.json());
-		app.use(cookieParser());
-		app.use(cors);
-		app.use(sessionFactory({}));
-		
-		app.set('views', __dirname + '/views');
-		app.set('view engine', 'ejs');
-		app.set('layout', 'layouts/html_app');
-		app.set('port', process.env.PORT || 8080);
-		app.use(expressLayouts);
-	},
+        GLOBAL.getModel = function(modelName) {
+            var Model = require(GLOBAL.MODEL_DIR + modelName);
+            return new Model();
+        };
 
-	start: function(app, express) {
-		this.setupGlobals();
-		logger.info('Loaded configuration: \n' + JSON.stringify(config));
-		this.setupApp(app, express);
-	}
+        GLOBAL.getDBConnection = function() {
+            var mysql = require('mysql');
+            var dbconfig = GLOBAL.config.database;
+            var connection = mysql.createConnection(dbconfig);
+            return connection;
+        };
+
+        GLOBAL.generatePasswordHash = function(password, salt) {
+            var shasum = GLOBAL.getSHA1();
+            shasum.update(salt + password + salt);
+            var hash = shasum.digest('hex');
+            return hash;
+        };
+
+        GLOBAL.getCrypt = function() {
+            return require('crypto');
+        };
+
+        GLOBAL.getSHA1 = function() {
+            var crypt = GLOBAL.getCrypt();
+            var shasum = crypt.createHash('sha1');
+            return shasum;
+        };
+
+        GLOBAL.getValidator = function() {
+            var validator = require('validator');
+            validator.extend('isPassword', function(str) {
+                // TODO: Finish writing the regex to test passwords
+                if (!str) {
+                    return false;
+                }
+                return true;
+                //return /^$/.test(str);
+            });
+            return new Validator();
+        };
+
+        GLOBAL.getDateFormatter = function() {
+            return require('dateformat');
+        };
+        //Starting Logger;
+        GLOBAL.logger = require('./lib/logger');
+    }
 };
--- index.js
+++ index.js
@@ -2,21 +2,41 @@
 
 var express = require('express');
 var app = express();
-
 var bootstrap = require('./bootstrap.js');
+
+//Add GLOBAL;
+bootstrap.init();
+
 var routesApi = require('./app/routes-api');
 var routesWeb = require('./app/routes-web');
 
-
-
-
-bootstrap.start(app, express);
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var cors = require('./lib/cors');
+var sessionFactory = require('./lib/sessionFactory');
+var expressLayouts = require('express-ejs-layouts');
 
 routesWeb.setup(app);
 routesApi.setup(app);
 
+app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors);
+app.use(sessionFactory({}));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('layout', 'layouts/html_app');
+app.set('port', config.server.port);
+app.use(expressLayouts);
 
 app.listen(app.get('port'), function() {
+    logger.info('Loaded configuration: \n' + getUtil.inspect(config));
     logger.info('Server started.');
     logger.info('Listening on port: ' + app.get('port'));
 });
