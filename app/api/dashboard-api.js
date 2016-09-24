af08db18465b5f34829e5f3029a178a9480c02aa
Jerum Hubbert <hubbertj@gmail.com>
2016-09-23 19:43:38 -0700

    completed dashboard report backend

--- app/api/dashboard-api.js
+++ app/api/dashboard-api.js
@@ -9,7 +9,7 @@
 function DashboardApi() {
     this.tag = 'dashboard-api';
 
-    /* duration = 'week' | 'day' | '5day' | 'month' | '3month' | 'year' | '5year' | 'max';
+    /* duration = 'week' | 'day' | 'month'  | 'year' | 'max';
         {
             duration: string,
             timeZone: string ex. pst
@@ -25,19 +25,20 @@ function DashboardApi() {
         var Patron = models.employee_user;
 
         var currentTime = new Date();
-        if (!req.query.duration || !req.query.ts) {
+        if (!req.query.duration) {
             this.getErrorApi().sendError(1012, 400, res);
             return;
         } else {
             _duration = req.query.duration;
         }
+
         switch (_duration) {
-             case 'day':
+            case 'day':
                 var patronCount = {};
                 for (var x = 0; x < _numOfOutput; x++) {
-                    var d = new Date().setHours(new Date().getHours() - (x * 3.4));
-                    var ts = new Date().setDate(d);
-                    patronCount[x] = ts;
+                    var d = new Date();
+                    var date = d.setHours(d.getHours() - (x * 3.4));
+                    patronCount[x] = date;
                 }
 
                 Patron.findAndCountAll({
@@ -327,10 +328,108 @@ function DashboardApi() {
             case 'year':
                 var patronCount = {};
                 for (var x = 0; x < _numOfOutput; x++) {
-                    var ts = new Date().setDate(new Date().getMonth() - x );
-                    patronCount[x] = ts;
+                    var d = new Date();
+                    var date = d.setMonth(d.getMonth() - x);
+                    patronCount[x] = date;
                 }
 
+                Patron.findAndCountAll({
+                    where: {
+                        createdAt: {
+                            $lte: new Date(patronCount[0])
+                        }
+                    }
+                }).then(function(result) {
+                    patronCount[0] = {
+                        date_ts: patronCount[0],
+                        count: result.count
+                    }
+                    return Patron.findAndCountAll({
+                        where: {
+                            createdAt: {
+                                $lte: new Date(patronCount[1])
+                            }
+                        }
+                    });
+                }).then(function(result) {
+                    patronCount[1] = {
+                        date_ts: patronCount[1],
+                        count: result.count
+                    }
+                    return Patron.findAndCountAll({
+                        where: {
+                            createdAt: {
+                                $lte: new Date(patronCount[2])
+                            }
+                        }
+                    });
+                }).then(function(result) {
+                    patronCount[2] = {
+                        date_ts: patronCount[2],
+                        count: result.count
+                    }
+                    return Patron.findAndCountAll({
+                        where: {
+                            createdAt: {
+                                $lte: new Date(patronCount[3])
+                            }
+                        }
+                    });
+                }).then(function(result) {
+                    patronCount[3] = {
+                        date_ts: patronCount[3],
+                        count: result.count
+                    }
+                    return Patron.findAndCountAll({
+                        where: {
+                            createdAt: {
+                                $lte: new Date(patronCount[4])
+                            }
+                        }
+                    });
+                }).then(function(result) {
+                    patronCount[4] = {
+                        date_ts: patronCount[4],
+                        count: result.count
+                    }
+                    return Patron.findAndCountAll({
+                        where: {
+                            createdAt: {
+                                $lte: new Date(patronCount[5])
+                            }
+                        }
+                    });
+                }).then(function(result) {
+                    patronCount[5] = {
+                        date_ts: patronCount[5],
+                        count: result.count
+                    }
+                    return Patron.findAndCountAll({
+                        where: {
+                            createdAt: {
+                                $lte: new Date(patronCount[6])
+                            }
+                        }
+                    });
+                }).then(function(result) {
+                    patronCount[6] = {
+                        date_ts: patronCount[6],
+                        count: result.count
+                    }
+                    res.status(200).json({
+                        patronCount: patronCount,
+                        success: true
+                    });
+                });
+                break;
+
+            case 'max':
+                var patronCount = {};
+                for (var x = 0; x < _numOfOutput; x++) {
+                    var d = new Date();
+                    var date = d.setFullYear(d.getFullYear() - x);
+                    patronCount[x] = date;
+                }
                 Patron.findAndCountAll({
                     where: {
                         createdAt: {
@@ -425,9 +524,6 @@ function DashboardApi() {
                 this.getErrorApi().sendError(1012, 400, res);
                 break;
         }
-
-
-
     }
 }
 
