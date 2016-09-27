9b7cb62433483e4b7464d3f10f6c6c49efb61a4d
Jerum Hubbert <hubbertj@gmail.com>
2016-09-26 19:08:50 -0700

    formate code

--- migrations/20160926231604-adding-isActive-migration.js
+++ migrations/20160926231604-adding-isActive-migration.js
@@ -3,56 +3,54 @@
 module.exports = {
     up: function(queryInterface, Sequelize) {
         return queryInterface.addColumn(
-                'employee_user',
+            'employee_user',
+            'is_active', {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: true,
+                comment: "Is the employee active or not"
+            }
+        ).then(function(result) {
+            return queryInterface.addColumn(
+                'game',
                 'is_active', {
                     type: Sequelize.BOOLEAN,
                     allowNull: false,
                     defaultValue: true,
                     comment: "Is the employee active or not"
                 }
-            ).then(function(result) {
-                return queryInterface.addColumn(
-                    'game',
-                    'is_active', {
-                        type: Sequelize.BOOLEAN,
-                        allowNull: false,
-                        defaultValue: true,
-                        comment: "Is the employee active or not"
-                    }
-                );
-            }).then(function(result) {
-                return queryInterface.addColumn(
-                    'organization',
-                    'is_active', {
-                        type: Sequelize.BOOLEAN,
-                        allowNull: false,
-                        defaultValue: true,
-                        comment: "Is the employee active or not"
-                    }
-                );
-            }).then(function(result) {
-                return queryInterface.addColumn(
-                    'patron_player',
-                    'is_active', {
-                        type: Sequelize.BOOLEAN,
-                        allowNull: false,
-                        defaultValue: true,
-                        comment: "Is the employee active or not"
-                    }
-                );
-            });
+            );
+        }).then(function(result) {
+            return queryInterface.addColumn(
+                'organization',
+                'is_active', {
+                    type: Sequelize.BOOLEAN,
+                    allowNull: false,
+                    defaultValue: true,
+                    comment: "Is the employee active or not"
+                }
+            );
+        }).then(function(result) {
+            return queryInterface.addColumn(
+                'patron_player',
+                'is_active', {
+                    type: Sequelize.BOOLEAN,
+                    allowNull: false,
+                    defaultValue: true,
+                    comment: "Is the employee active or not"
+                }
+            );
+        });
     },
 
     down: function(queryInterface, Sequelize) {
-        return [
-            queryInterface.removeColumn('employee_user', 'is_active')
+        return queryInterface.removeColumn('employee_user', 'is_active')
             .then(function(result) {
                 return queryInterface.removeColumn('game', 'is_active')
             }).then(function(result) {
                 return queryInterface.removeColumn('organization', 'is_active')
             }).then(function(result) {
                 return queryInterface.removeColumn('patron_player', 'is_active')
-            })
-        ];
+            });
     }
 };
