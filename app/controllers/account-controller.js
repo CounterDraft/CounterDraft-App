6d67c899613da96a89a5d83b812a70236a0f835e
Jerum Hubbert <hubbertj@gmail.com>
2016-10-05 21:44:07 -0700

    adding stuff into router

--- app/controllers/account-controller.js
+++ app/controllers/account-controller.js
@@ -2,97 +2,81 @@ function AccountController() {
     this.tag = 'accountController';
 
     this.ApiRouter = {
-        login: function(verb, req, res) {
+        login: function(verb, req, res, self) {
             switch (verb) {
-                // @post(/api/login)
                 case 'post':
                     getApi('login-api').login(req, res);
                     break;
-                    // @get(/api/login)
                 case 'get':
                     getApi('login-api').getLogin(req, res);
                     break;
-                    // @put(/api/login) 
                 case 'put':
-                    getApi('error-api').sendError(1011, 403, res);
-                    break;
-                    // @delete(/api/login)     
+                    self.getErrorApi().sendError(1011, 403, res);
+                    break;    
                 case 'delete':
-                    getApi('error-api').sendError(1011, 403, res);
+                    self.getErrorApi().sendError(1011, 403, res);
                     break;
                 default:
-                    getApi('error-api').sendError(1011, 403, res);
+                    self.getErrorApi().sendError(1011, 403, res);
                     break;
             }
         },
 
-        registration: function(verb, req, res) {
-            switch (verb) {
-                // @post(/api/register)  
+        registration: function(verb, req, res, self) {
+            switch (verb) { 
                 case 'post':
                     getApi('registration-api').registerUser(req, res);
                     break;
-                    // @get(/api/register)     
                 case 'get':
                     getApi('registration-api').getUserRegistration(req, res);
                     break;
-                    // @put(/api/register)       
                 case 'put':
-                    getApi('error-api').sendError(1011, 403, res);
-                    break;
-                    // @delete(/api/register)     
+                    self.getErrorApi().sendError(1011, 403, res);
+                    break;  
                 case 'delete':
-                    getApi('error-api').sendError(1011, 403, res);
+                    self.getErrorApi().sendError(1011, 403, res);
                     break;
                 default:
-                    getApi('error-api').sendError(1011, 403, res);
+                    self.getErrorApi().sendError(1011, 403, res);
                     break;
             }
 
         },
-        verify: function(verb, req, res) {
+        verify: function(verb, req, res, self) {
             switch (verb) {
-                // @post(/api/verify) 
                 case 'post':
-                    getApi('error-api').sendError(1011, 403, res);
-                    break;
-                    // @get(/api/verify)    
+                    self.getErrorApi().sendError(1011, 403, res);
+                    break;    
                 case 'get':
-                    getApi('error-api') sendError(1011, 403, res);
-                    break;
-                    // @put(/api/verify)     
+                    self.getErrorApi().sendError(1011, 403, res);
+                    break;   
                 case 'put':
-                    getApi('error-api').sendError(1011, 403, res);
-                    break;
-                    // @delete(/api/verify)     
+                    self.getErrorApi().sendError(1011, 403, res);
+                    break; 
                 case 'delete':
-                    getApi('error-api').sendError(1011, 403, res);
+                    self.getErrorApi().sendError(1011, 403, res);
                     break;
                 default:
-                    getApi('error-api').sendError(1011, 403, res);
+                    self.getErrorApi().sendError(1011, 403, res);
                     break;
             }
         },
         default: function(verb, req, res) {
             switch (verb) {
-                // @post(/api/verify) 
                 case 'post':
-                    getApi('error-api').sendError(1011, 403, res);
-                    break;
-                    // @get(/api/verify)    
+                    self.getErrorApi().sendError(1011, 403, res);
+                    break; 
                 case 'get':
-                    getApi('error-api') sendError(1011, 403, res);
-                    break;
-                    // @put(/api/verify)     
+                    self.getErrorApi().sendError(1011, 403, res);
+                    break;    
                 case 'put':
-                    getApi('error-api').sendError(1011, 403, res);
-                    break;
-                    // @delete(/api/verify)     
+                    self.getErrorApi().sendError(1011, 403, res);
+                    break;   
                 case 'delete':
-                    getApi('error-api').sendError(1011, 403, res);
+                    self.getErrorApi().sendError(1011, 403, res);
                     break;
                 default:
-                    getApi('error-api').sendError(1011, 403, res);
+                    self.getErrorApi().sendError(1011, 403, res);
                     break;
             }
         }
--- app/controllers/application-controller.js
+++ app/controllers/application-controller.js
@@ -2,64 +2,60 @@ function AccountController() {
     this.tag = 'accountController';
 
     this.ApiRouter = {
-        organization: function(verb, req, res) {
+        organization: function(verb, req, res, self) {
             switch (verb) {
                 case 'get':
                     getApi('organization-api').getOrganizationTypes(req, res);
                     break;
                 case 'post':
-                    getApi('error-api').sendError(1011, 403, res);
+                    self.getErrorApi().sendError(1011, 403, res);
                     break;
                 case 'put':
-                    getApi('error-api').sendError(1011, 403, res);
+                    self.getErrorApi().sendError(1011, 403, res);
                     break;
                 case 'delete':
-                    getApi('error-api').sendError(1011, 403, res);
+                    self.getErrorApi().sendError(1011, 403, res);
                     break;
                 default:
-                    getApi('error-api').sendError(1011, 403, res);
+                    self.getErrorApi().sendError(1011, 403, res);
                     break;
             }
         },
-        reset: function(verb, req, res) {
+        reset: function(verb, req, res, self) {
             switch (verb) {
                 case 'get':
-                    getApi('error-api').sendError(1011, 403, res);
+                    self.getErrorApi().sendError(1011, 403, res);
                     break;
                 case 'post':
-                    getApi('error-api').sendError(1011, 403, res);
+                    self.getErrorApi().sendError(1011, 403, res);
                     break;
                 case 'put':
                     getApi('reset-api').resetUsernamePassword(req, res);
                     break;
                 case 'delete':
-                    getApi('error-api').sendError(1011, 403, res);
+                    self.getErrorApi().sendError(1011, 403, res);
                     break;
                 default:
-                    getApi('error-api').sendError(1011, 403, res);
+                    self.getErrorApi().sendError(1011, 403, res);
                     break;
             }
         },
-        default: function(verb, req, res) {
+        default: function(verb, req, res,self) {
             switch (verb) {
-                // @post(/api/verify) 
                 case 'post':
-                    getApi('error-api').sendError(1011, 403, res);
+                    self.getErrorApi().sendError(1011, 403, res);
                     break;
-                    // @get(/api/verify)    
                 case 'get':
-                    getApi('error-api') sendError(1011, 403, res);
-                    break;
-                    // @put(/api/verify)     
+                    self.getErrorApi().sendError(1011, 403, res);
+                    break;  
                 case 'put':
-                    getApi('error-api').sendError(1011, 403, res);
-                    break;
-                    // @delete(/api/verify)     
+                    self.getErrorApi().sendError(1011, 403, res);
+                    break;   
                 case 'delete':
-                    getApi('error-api').sendError(1011, 403, res);
+                    self.getErrorApi().sendError(1011, 403, res);
                     break;
                 default:
-                    getApi('error-api').sendError(1011, 403, res);
+                    self.getErrorApi().sendError(1011, 403, res);
                     break;
             }
         }
