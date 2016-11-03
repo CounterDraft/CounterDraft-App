b6bb444cee803f1620ffd62ce75acc2b4d5819b8
Jerum Hubbert <jerum.hubbert@scientificgames.com>
2016-11-02 23:45:53 -0700

    added grph stuff

--- js/controllers/dashboardCtrl.js
+++ js/controllers/dashboardCtrl.js
@@ -18,10 +18,22 @@ app.controller('DashCtrl', ['$scope', '$http', '$window', 'data', function($scop
     //options;
     $scope.timeSpan = ['none'];
 
+    $scope.chartData = null;
+
     //quick data;
     $scope.total_patrons = 0;
     $scope.daily_active_users = 0;
     $scope.active_games = 0;
+
+    // line-data='[
+    //         { y: "2010", a: 100, b: 90 },
+    //         { y: "2011", a: 75,  b: 65 },
+    //         { y: "2012", a: 50,  b: 40 },
+    //         { y: "2013", a: 75,  b: 65 },
+    //         { y: "2014", a: 50,  b: 40 },
+    //         { y: "2015", a: 75,  b: 65 },
+    //         { y: "2016", a: 99, b: 99 }
+    //     ]'
 
     //models
     $scope.user_model = { username: 'anonymous' };
@@ -39,7 +51,7 @@ app.controller('DashCtrl', ['$scope', '$http', '$window', 'data', function($scop
             if (data.timeSpans) {
                 $scope.timeSpan = data.timeSpans;
                 $scope.timeSelect = $scope.timeSpan[$scope.timeSpan.length - 1];
-                this.onTimeChange();
+                this.onTimeChange($scope.timeSpan[$scope.timeSpan.length - 1]);
             }
         }
 
@@ -70,29 +82,31 @@ app.controller('DashCtrl', ['$scope', '$http', '$window', 'data', function($scop
         });
     }
 
-    this.onTimeChange = function() {
+    this.onTimeChange = function(duration) {
         var formData = {
-            duration: $scope.timeSelect
+            duration: duration
         }
+        
+        var patronData = null;
+        var gameData = null;
 
         $http({
             method: 'GET',
             url: _dashboard_patron_url,
             params: formData
         }).then(function successCallback(response) {
-            // console.log(response.data);
+            patronData = response.data;
             return $http({
                 method: 'GET',
                 url: _dashboard_game_url,
                 params: formData
             });
-        }, function errorCallback(response) {
-            // console.error(response);
         }).then(function successCallback(response) {
-            // console.log(response.data);
+            gameData = response.data;
+            _updateLineGraph(patronData.counts, gameData.counts);
 
         }, function errorCallback(response) {
-            // console.error(response);
+            console.error(response);
         });
     }
 
@@ -100,6 +114,34 @@ app.controller('DashCtrl', ['$scope', '$http', '$window', 'data', function($scop
         if (page) {
             $scope.currentPage = _base_templates + page + '.html';
         }
+    }
+
+    var _updateLineGraph = function(patronCountObj, gameCountObj) {
+        var newData = [];
+        var patronData = [];
+        var gameData = [];
+        var yData = [];
+
+        var duration = angular.copy($scope.timeSelect);
+
+        angular.forEach(patronCountObj, function(value, key) {
+            var date = moment(value.date_ts).format('MM-DD-YYYY');
+            var count = value.count;
+            yData.push(date);
+            patronData.push(count);
+        });
+
+        angular.forEach(gameCountObj, function(value, key) {
+            var date = moment(value.date_ts).format('MM-DD-YYYY');
+            var count = value.count;
+            gameData.push(count);
+        });
+
+        angular.forEach(yData, function(value, key) {
+            newData.push({ y: yData[key], patrons: patronData[key], games: gameData[key] });
+        });
+
+        $scope.chartData = newData;
     }
 
     var _getDefaultPage = function() {
